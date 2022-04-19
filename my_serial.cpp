42eebed30d922581470d1c02841239170b718c4c
Ziloansel <1742975837@qq.com>
2022-04-19 19:47:19 +0800

    实现简单的自发自收

---
+++ my_serial.cpp
@@ -0,0 +1,62 @@
+
+#include <string>
+#include <iostream>
+#include <cstdio>
+#include<exception>
+
+// OS Specific sleep
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+
+#include "serial/serial.h"
+
+using std::string;
+using std::exception;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::vector;
+using std::cin;
+
+void my_sleep(unsigned long milliseconds) {
+#ifdef _WIN32
+      Sleep(milliseconds); // 100 ms
+#else
+      usleep(milliseconds*1000); // 100 ms
+#endif
+}
+
+void run()
+{
+    serial::Serial a("/dev/ttyUSB0",114500,serial::Timeout::simpleTimeout(1000));
+    if(!a.isOpen()){
+        throw("串口没打开");
+    }
+    int num{};
+    for(int i=0;i<3;i++)
+    {
+        cout<<i<<endl;
+        cout<<"请输入串口发送信息"<<endl;
+        string send;
+        cin>>send;
+        a.write(send);
+        cout<<"接受到的信息为"<<a.read(1024)<<endl;
+        
+    }
+
+}
+
+
+int main(int argc, char **argv) {
+    try{
+        run();
+    }catch (exception &e) {
+        cout<<e.what()<<endl;
+    }
+    
+    return 0;
+}
